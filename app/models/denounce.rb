f2387742272956a4476fe75cb3c66d897742e3b3
Jan Chruszcz <jrchruszcz@gmail.com>
2017-08-11 16:43:26 +0200

    Denounce type model created

--- app/models/denounce.rb
+++ app/models/denounce.rb
@@ -3,5 +3,6 @@ class Denounce < ApplicationRecord
 	belongs_to :author_user, class_name: "User"
 	belongs_to :denounced_user, class_name: "User"
 	belongs_to :town
+	belongs_to :denounce_type
 
 end
