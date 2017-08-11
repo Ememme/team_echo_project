e413305def00825819341596168f05c191a70885
Jan Chruszcz <jrchruszcz@gmail.com>
2017-08-11 16:32:36 +0200

    Denounce type model and migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811141312) do
+ActiveRecord::Schema.define(version: 20170811142926) do
+
+  create_table "denounce_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "denounces", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "content"
