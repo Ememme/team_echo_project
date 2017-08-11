0cac896f37776663d39990379f0dfe0c10aad829
Piotr Halkiewicz <halkye@gmail.com>
2017-08-11 14:50:11 +0200

    Added a denounce mailer

--- app/mailers/denounce_mailer.rb
+++ app/mailers/denounce_mailer.rb
@@ -1,2 +1,8 @@
 class DenounceMailer < ApplicationMailer
+  default from: "mailerforncc@gmail.com"
+
+    def denounce_email(user)
+    @user = user
+    mail(to: @user.email, subject: "You've been denounced")
+  end
 end
