53c133fd8608f3124f888f84e0c160249993aef4
Piotr Halkiewicz <halkye@gmail.com>
2017-08-14 15:59:59 +0200

    Add form for new denounce (which is not working fully yet)

--- app/controllers/denounces_controller.rb
+++ app/controllers/denounces_controller.rb
@@ -1,74 +1,60 @@
 class DenouncesController < ApplicationController
   before_action :set_denounce, only: [:show, :edit, :update, :destroy]
 
-  # GET /denounces
-  # GET /denounces.json
+
   def index
     @denounces = Denounce.all
   end
 
-  # GET /denounces/1
-  # GET /denounces/1.json
-  def show
-  end
-
-  # GET /denounces/new
   def new
     @denounce = Denounce.new
+    @users = User.all
+    @towns = Town.all
+    @denounce_types = DenounceType.all
   end
 
-  # GET /denounces/1/edit
-  def edit
-  end
-
-  # POST /denounces
-  # POST /denounces.json
   def create
     @denounce = Denounce.new(denounce_params)
 
-    respond_to do |format|
-      if @denounce.save
-        format.html { redirect_to @denounce, notice: 'Denounce was successfully created.' }
-        format.json { render :show, status: :created, location: @denounce }
-      else
-        format.html { render :new }
-        format.json { render json: @denounce.errors, status: :unprocessable_entity }
-      end
+    if @denounce.save
+      redirect_to @denounce, notice: t("denounce_created")
+    else
+      @users = User.all
+      @towns = Town.all
+      @denounce_types = DenounceType.all
+      render :new
     end
   end
 
-  # PATCH/PUT /denounces/1
-  # PATCH/PUT /denounces/1.json
   def update
-    respond_to do |format|
-      if @denounce.update(denounce_params)
-        format.html { redirect_to @denounce, notice: 'Denounce was successfully updated.' }
-        format.json { render :show, status: :ok, location: @denounce }
-      else
-        format.html { render :edit }
-        format.json { render json: @denounce.errors, status: :unprocessable_entity }
-      end
+    if @denounce.update(denounce_params)
+      redirect_to @denounce, notice: t("denounce_updated")
+    else
+      @users = User.all
+      @towns = Town.all
+      @denounce_types = DenounceType.all
+      render :edit
     end
   end
 
-  # DELETE /denounces/1
-  # DELETE /denounces/1.json
   def destroy
     @denounce.destroy
-    respond_to do |format|
-      format.html { redirect_to denounces_url, notice: 'Denounce was successfully destroyed.' }
-      format.json { head :no_content }
+    respond_to do
+      redirect_to denounces_url, notice: t("denounce_destroyed")
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_denounce
       @denounce = Denounce.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def denounce_params
-      params.require(:denounce).permit(:content)
+      params.require(:denounce).permit(:content, 
+                                       :author_user_id,
+                                       :denounced_user_id,
+                                       :mail,
+                                       :town_id )
     end
 end
