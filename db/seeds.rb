20634b51dfad89fdac1bed3fa44aaededafbb6bf
mpiask <mpiask@gmail.com>
2017-08-14 15:10:23 +0200

    new seeds file
    generates 1 town, 3 dn types, 100 users and denounces

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
 
-	town = Town.create(name: "Poznań")
+	# User.delete_all
+	# Denounce.delete_all
+
+	# Town.delete_all
+	Town.create(id: 1, name: "Poznań")
+	# DenounceType.delete_all
+	DenounceType.create(id: 1, value: 10) #anonim
+	DenounceType.create(id: 2, value: 30) #wyświetlony nick
+	DenounceType.create(id: 3, value: 50) #wyświetlone nazwisko
+
+	100.times do
+		print ','
+		user = User.create(name: FFaker::NamePL.name,
+			nick: FFaker::BaconIpsum.word,
+			email: FFaker::Internet.email,
+			password: 'password',
+			password_confirmation: 'password'
+		)
+		rand(10).times do
+			print '.'
+			Denounce.create(
+				content: FFaker::BaconIpsum.sentence,
+				author_user_id: user.id,
+				denounced_user_id: rand(user.id)+1,
+				town_id: 1,
+				denounce_type_id: rand(3)+1,
+				created_at: rand(1000).hours.ago
+			)
+		end
+	end
