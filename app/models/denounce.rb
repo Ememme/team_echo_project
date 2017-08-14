d334f6d56fc3dab281aa0600bb54d58135e0b186
mpiask <mpiask@gmail.com>
2017-08-14 15:10:46 +0200

    denounce feed on home page

--- app/models/denounce.rb
+++ app/models/denounce.rb
@@ -4,5 +4,15 @@ class Denounce < ApplicationRecord
 	belongs_to :denounced_user, class_name: "User"
 	belongs_to :town
 	belongs_to :denounce_type
+  default_scope -> { order(created_at: :desc) }
 
+	def show_author
+		if self.denounce_type_id == 3
+			self.author_user.name
+		elsif self.denounce_type_id == 2
+			'@' + self.author_user.nick
+		else
+			'@anonim'
+		end
+	end
 end
