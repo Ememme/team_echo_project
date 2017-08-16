286b67018d4c70f548311be7c56458caa3992757
Kristopher Michalski <kristopher.michalski+git@gmail.com>
2017-08-16 13:46:12 +0200

    Fix root_path helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,21 @@ class ApplicationController < ActionController::Base
     UserDecorator.decorate(super) unless super.nil?
   end
 
+  def root_path
+    if user_signed_in?
+      authenticated_root_path
+    else
+      unauthenticated_root_path
+    end
+  end
+
+  def root_url
+    if user_signed_in?
+      authenticated_root_url
+    else
+      unauthenticated_root_url
+    end
+  end
+
+  helper_method :root_path, :root_url
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-  	if current_user.id == @user.id
-      @reported_denounces = @user.reported_denounces.paginate(:page => params[:rep_page], :per_page => 10).decorate
-      @received_denounces = @user.received_denounces.paginate(:page => params[:rec_page], :per_page => 10).decorate
-	else
-	  redirect_to root_path
-	end
+
+    if current_user.id == @user.id
+      @reported_denounces = @user.reported_denounces.paginate(page: params[:rep_page], per_page: 10).decorate
+      @received_denounces = @user.received_denounces.paginate(page: params[:rec_page], per_page: 10).decorate
+    else
+      redirect_to root_path
+    end
   end
+
   def ranking
     @users = User.order('points DESC').limit(20).decorate
   end
