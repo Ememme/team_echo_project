9d1e88a551032cade4c266f82e08c622efebdc42
mpiask <mpiask@gmail.com>
2017-08-16 11:50:52 +0200

    migrate add_score_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812113315) do
+ActiveRecord::Schema.define(version: 20170816094708) do
 
   create_table "denounce_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170812113315) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "nick"
+    t.integer "points", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
