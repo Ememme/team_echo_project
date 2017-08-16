e87999b0a63f9c7f0d4aadb730d989824ea5da99
Piotr Halkiewicz <halkye@gmail.com>
2017-08-16 09:28:01 +0200

    added config for slack and custom message

--- app/controllers/denounces_controller.rb
+++ app/controllers/denounces_controller.rb
@@ -16,8 +16,17 @@ class DenouncesController < ApplicationController
   def create
     @denounce = Denounce.new(denounce_params)
     @denounce.author_user = current_user
+    notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+      defaults channel: "#general",
+               username: "denounce-bot"
+    end
     if @denounce.save
       redirect_to denounces_path, notice: t("denounce_created")
+      notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+        defaults channel: "#general",
+                 username: "denounce-bot"
+      notifier.ping "You've been denounced"
+end
     else
       @users = User.all
       @towns = Town.all
