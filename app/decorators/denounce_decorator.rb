43d571f962e6559587940e876c42cb1939ad76a8
Kristopher Michalski <kristopher.michalski+git@gmail.com>
2017-08-17 14:42:01 +0200

    Split into 2 methods for rubocop

--- app/decorators/denounce_decorator.rb
+++ app/decorators/denounce_decorator.rb
@@ -2,12 +2,16 @@ class DenounceDecorator < ApplicationDecorator
   delegate_all
 
   def show_author
-    if object.denounce_type_id == 3
-      object.author_user.decorate.nick_or_name_or_email
-    elsif object.denounce_type_id == 2
-      object.author_user.decorate.nick_or_name_or_email(skip_name: true)
-    else
-      '@anonim'
+    author_name_or_nick || I18n.t('anon')
+  end
+
+  private
+
+  def author_name_or_nick
+    if object.denounce_type.gid == 3
+      object.author_user.name
+    elsif object.denounce_type.gid == 2
+      object.author_user.nick
     end
   end
 end
