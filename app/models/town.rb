02b4ed9ca562732b2b56e782676117ac3e69e19c
Kristopher Michalski <kristopher.michalski+git@gmail.com>
2017-08-17 14:47:40 +0200

    Fix tests

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,3 +1,11 @@
 class Town < ApplicationRecord
-  has_many :denounces
+  has_many :denounces, dependent: :destroy
+
+  def self.first_or_one
+    if table_exists? && first.present?
+      first.id
+    else
+      1
+    end
+  end
 end
--- spec/factories/denounce_types.rb
+++ spec/factories/denounce_types.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :denounce_type do
-    name "MyString"
     value 1
   end
 end
