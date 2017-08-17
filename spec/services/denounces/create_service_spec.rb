8b56780f1ac230978c4d2a4d668dec02db4dc1e7
Piotr Halkiewicz <halkye@gmail.com>
2017-08-17 11:15:50 +0200

    removed slack ping from denounce spec

--- spec/services/denounces/create_service_spec.rb
+++ spec/services/denounces/create_service_spec.rb
@@ -9,15 +9,6 @@ describe Denounces::CreateService do
 
     subject { described_class.new(params, author, town).call }
 
-    context 'when params are valid' do
-      let(:params) do
-        {
-          denounced_user_id: denounced_user.id, 
-          denounce_type_id: denounce_type.id,
-          content: "rspec"
-        }
-      end
-
       it 'creates new denounce' do
         expect { subject }.to change { Denounce.count }.by(1)
         expect(Denounce.last.author_user).to eq author
